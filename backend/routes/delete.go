fe0c945b4f9e8a1e264b99f6aea1990e44fa7171
Ojas Singh <ojas.singh02@gmail.com>
2023-09-11 14:48:06 -0400

    bug fix

--- backend/routes/delete.go
+++ backend/routes/delete.go
@@ -13,6 +13,8 @@ import (
 
 func DeleteItem(c *gin.Context) {
 	itemID := c.Param("id")
+
+	// Convert itemID to BSON ObjectId
 	objectID, err := primitive.ObjectIDFromHex(itemID)
 	if err != nil {
 		// Handle the error, e.g., invalid item ID format
@@ -20,16 +22,27 @@ func DeleteItem(c *gin.Context) {
 		return
 	}
 
-	filter := bson.D{{"_id", objectID}}
 	var client = database.ConnectDB()
 	var collection = collection.GetCollection(client)
 
+	filter := bson.M{"_id": objectID}
+
+	// Delete the item from the database
 	result, err := collection.DeleteOne(context.TODO(), filter)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete item"})
+		// Handle the error, e.g., item not found
+		c.JSON(http.StatusNotFound, gin.H{"error": "Item not found"})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Item deleted successfully", "result": result})
+	if result.DeletedCount == 0 {
+		// If DeletedCount is 0, the item was not found
+		c.JSON(http.StatusNotFound, gin.H{"error": "Item not found"})
+		return
+	}
 
+	// Item successfully deleted
+	c.JSON(http.StatusOK, gin.H{"message": "Item deleted successfully"})
+
+	database.DisconnectDB(client)
 }
