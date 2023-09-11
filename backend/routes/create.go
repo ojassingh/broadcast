6281ff14ef9947283ed4769d1b49f08dd96e029f
Ojas Singh <ojas.singh02@gmail.com>
2023-09-11 14:16:35 -0400

    Completed Create function of CRUD

--- backend/routes/create.go
+++ backend/routes/create.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	collection "github.com/TutorialEdge/realtime-chat-go-react/Collection"
@@ -11,6 +12,7 @@ import (
 )
 
 func CreateItem(c *gin.Context) {
+
 	var newItem model.Item
 	if err := c.ShouldBindJSON(&newItem); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
@@ -20,13 +22,14 @@ func CreateItem(c *gin.Context) {
 	var client = database.ConnectDB()
 	var collection = collection.GetCollection(client)
 
-	_, err := collection.InsertOne(context.TODO(), newItem)
+	result, err := collection.InsertOne(context.TODO(), newItem)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println("Error binding JSON:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
 		return
 	}
 
-	c.JSON(http.StatusCreated, newItem)
+	c.JSON(http.StatusCreated, gin.H{"_id": result.InsertedID})
 
 	database.DisconnectDB(client)
 }
